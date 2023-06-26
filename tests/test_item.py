9bf128640b41d10fb19b1b19e1ae6cbd26c2ead9
nataliagovorushkina <a10061514@gmail.com>
2023-06-26 20:32:46 +0300

    Initial 13.1.1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,21 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+def test_calculate_total_price():
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+
+    item1.calculate_total_price()
+    assert item1.price * item1.quantity == 200000
+    item2.calculate_total_price()
+    assert item2.price * item2.quantity == 100000
+
+    # устанавливаем новый уровень цен и применяем скидки
+
+def test_apply_discount():
+    item1 = Item("Смартфон", 10000, 20)
+    Item.pay_rate = 0.8
+    item1.apply_discount()
+    assert item1.price == 8000.0
+
+
