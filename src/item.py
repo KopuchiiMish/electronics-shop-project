b33894c8e973bdd164187f130aa2aafca9d1deb6
nataliagovorushkina <a10061514@gmail.com>
2023-06-29 11:01:07 +0300

    homework3 14.1

--- src/item.py
+++ src/item.py
@@ -21,7 +21,10 @@ class Item:
         self.all.append(self)
 
     def __repr__(self):
-        return f'Item({self.name}, {self.price}, {self.quantity})'
+        return f"Item('{self.name}', {self.price}, {self.quantity})"
+
+    def __str__(self) -> str:
+        return self.name
 
     @property
     def name(self):
